b1c963b3b253d370ec4d35a34d1a3493139bb277
duelxl <duelxl@126.com>
2019-12-03 14:18:11 +0800

    新建实体类,提交到GIT

---
+++ GitTest/src/main/java/com/duelxl/pojo/User.java
@@ -0,0 +1,12 @@
+package com.duelxl.pojo;
+
+/**
+ * @author Duelxl
+ * @date 2019/12/3 14:14
+ */
+public class User {
+    public void sayHello(){
+
+        System.out.println("Hello,Git!");
+    }
+}
